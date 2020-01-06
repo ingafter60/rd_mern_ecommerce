57097137b5b87c5a5081ff59f225d730038babe8
ing <inyoman_gurnitha@yahoo.com>
2020-01-06 09:20:09 +0700

    03.3. User signup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 // import routes
@@ -15,6 +18,11 @@ mongoose
         useCreateIndex: true
     })
     .then(() => console.log("DB Connected"));
+
+// middlewares
+app.use(morgan("dev")); // to simplify the process of logging requests to the application
+app.use(bodyParser.json()); // to get json data from request body
+app.use(cookieParser()); // for user's credentials in cookies
 
 // routes middleware
 app.use("/api", userRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,16 @@
-exports.sayHi = (req, res) => {
-    res.json({ message: "hello there" });
+const User = require("../models/user");
+
+exports.signup = (req, res) => {
+    console.log("req.body", req.body);
+    const user = new User(req.body); // create new user from what we got from the request body
+    user.save((err, user) => { // get error or save user to db
+        if (err) { // if error
+            return res.status(400).json({
+                error
+            });
+        }
+        res.json({ // response (with json) after saving the newly users
+            user
+        });
+    });
 };
